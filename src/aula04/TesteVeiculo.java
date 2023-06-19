98a7b93f4788e38b14539e913d0d7037dae558c9
MunhosKat <matheus.munhos2005@gmail.com>
2023-06-18 22:13:30 -0300

    a

--- src/aula04/TesteVeiculo.java
+++ src/aula04/TesteVeiculo.java
@@ -6,7 +6,7 @@ public class TesteVeiculo {
 		
 		
 		Veiculo matheusVeiculo = new Veiculo ("kat","qwertyu","pequeno","hatch");
-		Veiculo matheusHeiculo = new Veiculo ("zed","qwertuy","grande","hatch");
+		Veiculo henriqueVeiculo = new Veiculo ("zed","qwertuy","grande","hatch");
 		
 		
 		//String numeroSerie,String placa, String porte,String modelo
@@ -15,7 +15,53 @@ public class TesteVeiculo {
 		
 		matheusVeiculo.statusVeiculo(1);
 		
-		System.out.println(matheusVeiculo.statusVeiculo(1));
+		
+		matheusVeiculo.acelerar(240);
+		
+		matheusVeiculo.freiar(20);
+		
+		System.out.println(matheusVeiculo.getPlaca());
+		System.out.println(henriqueVeiculo.getPlaca());
+		
+		
+		
+		
+		
+		
+		
+		//System.out.print(matheusVeiculo.acelerar(12));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	//	matheusVeiculo.acelerar(20);
+		
+		
+		//System.out.println(matheusVeiculo.velocidadeVeiculo);
+	//	System.out.println(matheusVeiculo.statusVeiculo);
+		
+		
 		
 		
 		
--- src/aula04/Veiculo.java
+++ src/aula04/Veiculo.java
@@ -3,16 +3,15 @@ package aula04;
 import java.util.Date;
 
 public class Veiculo {
-	
+
 	protected String numeroSerie, cor, placa, porte, modelo;
 	protected Date dataFabricacao;
 	protected int statusVeiculo;
-	protected double limiteVeiculo, valorVeiculo,velocidadeVeiculo;
+	protected double limiteVeiculo, valorVeiculo, velocidadeVeiculo;
 	private static long contador;
 
-	
-	public Veiculo(String numeroSerie,String placa, String porte,String modelo) {
-		//apagar o SUPER
+	public Veiculo(String numeroSerie, String placa, String porte, String modelo) {
+		// apagar o SUPER
 		this.numeroSerie = numeroSerie;
 		this.modelo = modelo;
 		this.placa = placa;
@@ -20,17 +19,49 @@ public class Veiculo {
 		this.dataFabricacao = new Date(System.currentTimeMillis());
 		contador++;
 	}
-	
 
 	public boolean statusVeiculo(int valor) {
+		statusVeiculo = valor;
+
 		if (valor == 1) {
-			if (this.statusVeiculo < valor) {
-				this.statusVeiculo -= valor;
-				return true;
-			}
+			return true;
 
 		}
 		return false;
+
+	}
+
+	public double acelerar(double acelerar) {
+
+		if (statusVeiculo == 1) {
+			if (acelerar > 0) {
+				this.velocidadeVeiculo = acelerar;
+			}
+		}
+		System.out.println(velocidadeVeiculo + " KM/H");
+		return acelerar;
+
+	}
+
+	public double freiar(double freiar) {
+
+		if (velocidadeVeiculo > 0) {
+			velocidadeVeiculo -= freiar;
+		}
+		System.out.println(velocidadeVeiculo + " KM/H");
+		return freiar;
+
+	}
+	
+	
+	
+	public static void exibirDados() {
+		try {
+			System.out.println();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -39,11 +70,49 @@ public class Veiculo {
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+
 	public static void exibirContador() {
 		System.out.println(contador);
 	}
-	
-	
-	
-	
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+	public void setDataFabricacao(Date dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+
 }
